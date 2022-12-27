db4f28c74d272c127b5934a315568b9368ee7051
lera <leramulugeta@gmail.com>
2022-12-28 00:09:58 +0300

    updates on the testCase model

--- src/main/java/model/TestCase.java
+++ src/main/java/model/TestCase.java
@@ -12,13 +12,5 @@ import lombok.NoArgsConstructor;
 public class TestCase {
 
     private String title;
-//    private String testCaseStatus;
-//    private String severity;
-//    private String priority;
-//    private String type = "Functional";
-//    private String layer = "Unit";
-//    private boolean isFlaky = false;
-//    private String behavior = "Positive";
-//    private String automationStatus = "Not automated";
 
 }
