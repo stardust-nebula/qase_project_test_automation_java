e89ca432f5888b79294e02e4e72f6fd4d0dcf997
lera <leramulugeta@gmail.com>
2022-12-27 22:28:29 +0300

    added few more tests

---
+++ src/main/java/service/ProjectSettingsPageService.java
@@ -0,0 +1,34 @@
+package service;
+
+import io.qameta.allure.Step;
+import page.ProjectSettingsPage;
+
+public class ProjectSettingsPageService {
+
+    private ProjectSettingsPage projectSettingsPage = new ProjectSettingsPage();
+    private ProjectsPageService projectsPageService = new ProjectsPageService();
+
+    @Step("Open Project settings page by project name")
+    public ProjectSettingsPage openProjectSettingsPageByProjectName(String projectName) {
+        projectsPageService
+                .clickOnMeatballsIconForProjectByName(projectName)
+                .clickOnSettingsOptionByProject(projectName);
+        return new ProjectSettingsPage();
+    }
+
+    @Step("Change Project information and save")
+    public ProjectSettingsPage changeProjectSettingsAndSave(String newDescription) {
+        projectSettingsPage
+                .enterNewDescription(newDescription)
+                .clickOnUpdateSettingsButton();
+        return new ProjectSettingsPage();
+    }
+
+    public boolean isAlertOnSuccessfulProjectUpdateAppears() {
+        return projectSettingsPage.isAlertOnSuccessfulProjectUpdateAppears();
+    }
+
+    public String getTextFromUpdateProjectSettingsSuccessAlert() {
+        return projectSettingsPage.getTextFromUpdateProjectSettingsSuccessAlert();
+    }
+}
---
+++ src/test/java/test/ProjectSettingsPageTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import model.Project;
+import model.User;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import page.ProjectSettingsPage;
+import service.*;
+
+public class ProjectSettingsPageTest extends BaseTest{
+
+    private static final String demoProjectName = "Demo Project";
+    private ProjectSettingsPageService projectSettingsPageService;
+
+    @BeforeClass
+    public void setUp() {
+        new LoginPageService().loginValidCredentials(new User());
+        new ProjectsPageService()
+                .clickOnMeatballsIconForProjectByName(demoProjectName)
+                .clickOnSettingsOptionByProject(demoProjectName);
+        projectSettingsPageService = new ProjectSettingsPageService();
+    }
+
+    @Test(testName = "Verify success alert appears on successful changing project's description")
+    public void verifySuccessAlertOnChangingProjectDescription(){
+        String newDescription = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.";
+        String expectedSuccessText = "Project settings were successfully updated!";
+        projectSettingsPageService.changeProjectSettingsAndSave(newDescription);
+        String actualSuccessText = projectSettingsPageService.getTextFromUpdateProjectSettingsSuccessAlert();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(projectSettingsPageService.isAlertOnSuccessfulProjectUpdateAppears());
+        softAssert.assertEquals(actualSuccessText, expectedSuccessText);
+        softAssert.assertAll();
+    }
+}
